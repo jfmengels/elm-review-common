f396d423751437de766a4c66eead7d553bf42354
Jeroen Engels <jfm.engels@gmail.com>
2020-07-27 18:50:54 +0200

    Add a fuzz test for module names

--- tests/NoExposingEverythingTest.elm
+++ tests/NoExposingEverythingTest.elm
@@ -1,7 +1,11 @@
 module NoExposingEverythingTest exposing (all)
 
+import Expect
+import Fuzz
 import NoExposingEverything exposing (rule)
+import Random
 import Review.Test
+import Shrink
 import Test exposing (Test, describe, test)
 
 
@@ -17,12 +21,12 @@ d = 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
-        , test "should report when a module exposes everything" <|
-            \() ->
-                """
-module A exposing (..)
-import B exposing (..)
-"""
+        , Test.fuzz2 genName genName "should report when a module exposes everything" <|
+            \nameModule nameImport ->
+                ("""
+module """ ++ nameModule ++ """ exposing (..)
+import """ ++ nameImport ++ """ exposing (..)
+""")
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
@@ -32,6 +36,47 @@ import B exposing (..)
                                 ]
                             , under = "(..)"
                             }
-                            |> Review.Test.atExactly { start = { row = 2, column = 19 }, end = { row = 2, column = 23 } }
+                            |> Review.Test.atExactly { start = { row = 2, column = String.length nameModule + 18 }, end = { row = 2, column = String.length nameModule + 22 } }
                         ]
         ]
+
+
+upperCase : Random.Generator Char
+upperCase =
+    Random.int 0 25
+        |> Random.map (\n -> Char.fromCode (n + 65))
+
+
+lowerCase : Random.Generator Char
+lowerCase =
+    Random.int 0 25
+        |> Random.map (\n -> Char.fromCode (n + 97))
+
+
+digit : Random.Generator Char
+digit =
+    Random.int 0 9
+        |> Random.map (\n -> Char.fromCode (n + 48))
+
+
+anyValidNameCharacter : Random.Generator Char
+anyValidNameCharacter =
+    Random.weighted
+        ( 1, Random.constant '_' )
+        [ ( 26, upperCase )
+        , ( 26, lowerCase )
+        , ( 10, digit )
+        ]
+        |> Random.andThen identity
+
+
+genName : Fuzz.Fuzzer String
+genName =
+    let
+        generator : Random.Generator String
+        generator =
+            Random.map2 (\first rest -> String.fromList (first :: rest))
+                upperCase
+                (Random.int 5 10 |> Random.andThen (\n -> Random.list n anyValidNameCharacter))
+    in
+    Fuzz.custom generator Shrink.noShrink
