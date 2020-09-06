3490a1aee5979faef19c231621f8152fe0773f71
Jeroen Engels <jfm.engels@gmail.com>
2020-09-06 09:36:13 +0200

    Add preview configuration

---
+++ preview/src/ReviewConfig.elm
@@ -0,0 +1,29 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import Review.Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
+    , NoMissingTypeExpose.rule
+    ]
