ddcf3b2094f98ed956a9b841c84dd495704fd954
Jeroen Engels <jfm.engels@gmail.com>
2021-02-04 22:05:33 +0100

    Init NoSimpleLetBody rule

--- preview/src/ReviewConfig.elm
+++ preview/src/ReviewConfig.elm
@@ -16,12 +16,14 @@ import NoImportingEverything
 import NoMissingTypeAnnotation
 import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
+import NoSimpleLetBody
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ NoExposingEverything.rule
+    , NoSimpleLetBody.rule
     , NoImportingEverything.rule []
     , NoMissingTypeAnnotation.rule
     , NoMissingTypeAnnotationInLetIn.rule
