614dd35919d9e40a3da8e728ef67f588c7ba5d0f
Jeroen Engels <jfm.engels@gmail.com>
2021-02-04 22:07:50 +0100

    Add failing test

--- tests/NoSimpleLetBodyTest.elm
+++ tests/NoSimpleLetBodyTest.elm
@@ -8,17 +8,19 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoSimpleLetBody"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report an error when let body is a simple function or value" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a = let b = 1
+    in b
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "b"
                             }
+                            |> Review.Test.atExactly { start = { row = 5, column = 5 }, end = { row = 5, column = 14 } }
                         ]
         ]
