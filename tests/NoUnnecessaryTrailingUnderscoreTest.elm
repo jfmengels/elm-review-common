3806c7635663aab7b3b6021b6ff24a9e8ff35ea3
Jeroen Engels <jfm.engels@gmail.com>
2021-02-06 00:49:07 +0100

    rename test

--- tests/NoUnnecessaryTrailingUnderscoreTest.elm
+++ tests/NoUnnecessaryTrailingUnderscoreTest.elm
@@ -8,7 +8,7 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoUnnecessaryTrailingUnderscore"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report an error when argument has unnecessary trailing _" <|
             \() ->
                 """module A exposing (..)
 a value_ = 1
