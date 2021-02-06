0d5d797c7ad25adc5c916fa205e07a47e4af8c63
Jeroen Engels <jfm.engels@gmail.com>
2021-02-06 09:37:24 +0100

    Add failing tests

--- tests/NoUnnecessaryTrailingUnderscoreTest.elm
+++ tests/NoUnnecessaryTrailingUnderscoreTest.elm
@@ -42,4 +42,37 @@ a if_ then_ else_ case_ of_ let_ in_ type_ module_ where_ import_ exposing_ as_ 
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
+        , test "should report an error when a top-level variable has a trailing _" <|
+            \() ->
+                """module A exposing (..)
+a_ = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "REPLACEME"
+                            , details = [ "REPLACEME" ]
+                            , under = "a_"
+                            }
+                        ]
+        , test "should not report an error when a top-level variable has a trailing _ whose name is like a reserved keyword" <|
+            \() ->
+                """module A exposing (..)
+if_ = 1
+then_ = 1
+else_ = 1
+case_ = 1
+of_ = 1
+let_ = 1
+in_ = 1
+type_ = 1
+module_ = 1
+where_ = 1
+import_ = 1
+exposing_ = 1
+as_ = 1
+port_ = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
         ]
