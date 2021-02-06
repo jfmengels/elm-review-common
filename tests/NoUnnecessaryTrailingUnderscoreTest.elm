caa4a1e1103b5020250b0c79792e4bd2b882faee
Jeroen Engels <jfm.engels@gmail.com>
2021-02-06 09:21:08 +0100

    Add failing test

--- tests/NoUnnecessaryTrailingUnderscoreTest.elm
+++ tests/NoUnnecessaryTrailingUnderscoreTest.elm
@@ -35,4 +35,11 @@ a value_ value = 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
+        , test "should not report an error when the name without _ is a reserved keyword" <|
+            \() ->
+                """module A exposing (..)
+a if_ then_ else_ case_ of_ let_ in_ type_ module_ where_ import_ exposing_ as_ port_ = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
         ]
