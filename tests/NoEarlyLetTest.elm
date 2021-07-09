8db946cd31286c7095e8b23306171569e6d2842f
Jeroen Engels <jfm.engels@gmail.com>
2021-07-09 23:02:50 +0200

    Add tests

--- tests/NoEarlyLetTest.elm
+++ tests/NoEarlyLetTest.elm
@@ -5,20 +5,45 @@ import Review.Test
 import Test exposing (Test, describe, test)
 
 
+
+-- TODO Keep computations done outside of lambdas there. It might be an optimization.
+
+
 all : Test
 all =
     describe "NoEarlyLet"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report a let declaration that could be computed in a branch" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a b c d =
+  let
+    z = 1
+  in
+  if b then
+    z
+  else
+    1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "z"
                             }
                         ]
+        , test "should not report a let declaration is used in multiple branches" <|
+            \() ->
+                """module A exposing (..)
+a b c d =
+  let
+    z = 1
+  in
+  if b then
+    z
+  else
+    z + 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
         ]
