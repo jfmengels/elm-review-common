b1b26c8a366c1f9e9cd24c49007df33fecceac13
Jeroen Engels <jfm.engels@gmail.com>
2021-10-29 10:05:40 +0200

    Add tests

--- tests/NoDeprecatedTest.elm
+++ tests/NoDeprecatedTest.elm
@@ -8,17 +8,29 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoDeprecated"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report an error when using a non-deprecated element" <|
+            \() ->
+                [ """module A exposing (..)
+import Other
+a = Other.normalFunction
+""", """module Other exposing (..)
+normalFunction = 1
+""" ]
+                    |> Review.Test.runOnModules rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when referencing a local function whose name contains 'deprecated'" <|
             \() ->
                 """module A exposing (..)
-a = 1
+somethingDeprecated = 1
+
+a = somethingDeprecated
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "REPLACEME"
+                            { message = "Found new usage of deprecated `somethingDeprecated`"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "somethingDeprecated"
                             }
                         ]
         ]
