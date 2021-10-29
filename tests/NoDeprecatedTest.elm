77e7ca439a796ae2947660f8e32de2f1411995f1
Jeroen Engels <jfm.engels@gmail.com>
2021-10-29 18:52:16 +0200

    Add failing test

--- tests/NoDeprecatedTest.elm
+++ tests/NoDeprecatedTest.elm
@@ -138,6 +138,19 @@ a (Deprecated value) = 1
                             }
                             |> Review.Test.atExactly { start = { row = 3, column = 5 }, end = { row = 3, column = 15 } }
                         ]
+        , test "should report an error when having a parameter whose name contains 'deprecated' (top-level declaration)" <|
+            \() ->
+                """module A exposing (..)
+a thingDeprecated = 1
+"""
+                    |> Review.Test.run (rule NoDeprecated.checkInName)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Found new usage of deprecated element"
+                            , details = [ "REPLACEME" ]
+                            , under = "thingDeprecated"
+                            }
+                        ]
         , test "should report an error when destructuring a field whose name contains 'deprecated' (top-level declaration)" <|
             \() ->
                 """module A exposing (..)
