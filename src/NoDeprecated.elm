3845044fd145a1f69f6c1815cf37e0d90727edb3
Jeroen Engels <jfm.engels@gmail.com>
2021-11-13 09:44:27 +0100

    Fix docs

--- src/NoDeprecated.elm
+++ src/NoDeprecated.elm
@@ -806,7 +806,9 @@ error range =
 
 {-| Find the first element that satisfies a predicate and return
 Just that element. If none match, return Nothing.
-find (\\num -> num > 5) [2, 4, 6, 8] == Just 6
+
+    find (\num -> num > 5) [ 2, 4, 6, 8 ] == Just 6
+
 -}
 find : (a -> Bool) -> List a -> Maybe a
 find predicate list =
