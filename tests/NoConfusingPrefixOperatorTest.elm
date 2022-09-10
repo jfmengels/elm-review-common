ca45178ab17a1b9f88b15483207119f7b3a8a7cc
Jeroen Engels <jfm.engels@gmail.com>
2022-09-10 14:39:48 +0200

    Add tests for all

--- tests/NoConfusingPrefixOperatorTest.elm
+++ tests/NoConfusingPrefixOperatorTest.elm
@@ -1,6 +1,5 @@
 module NoConfusingPrefixOperatorTest exposing (all)
 
-import Expect exposing (Expectation)
 import NoConfusingPrefixOperator exposing (rule)
 import Review.Test
 import Test exposing (Test, describe, test)
@@ -8,50 +7,54 @@ import Test exposing (Test, describe, test)
 
 all : Test
 all =
-    describe "NoConfusingPrefixOperator"
-        [ test "should not report fully-applied operators" <|
-            \() ->
-                """module A exposing (..)
+    describe "NoConfusingPrefixOperator" (baseTests ++ nonCommutativeOperatorTests)
+
+
+baseTests : List Test
+baseTests =
+    [ test "should not report fully-applied operators" <|
+        \() ->
+            """module A exposing (..)
 a = a < 1
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectNoErrors
-        , test "should not report operators not used in an application" <|
-            \() ->
-                """module A exposing (..)
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    , test "should not report operators not used in an application" <|
+        \() ->
+            """module A exposing (..)
 a = (<)
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectNoErrors
-        , test "should report an error when using a confusing operator with only a single argument" <|
-            \() ->
-                """module A exposing (..)
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    , test "should report an error when using a confusing operator with only a single argument" <|
+        \() ->
+            """module A exposing (..)
 a = (<) 1
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "(<)"
-                            }
-                        ]
-        , test "should report an error when using a confusing operator with 2 arguments" <|
-            \() ->
-                """module A exposing (..)
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = "REPLACEME"
+                        , details = [ "REPLACEME" ]
+                        , under = "(<)"
+                        }
+                    ]
+    , test "should report an error when using a confusing operator with 2 arguments" <|
+        \() ->
+            """module A exposing (..)
 a = (<) 1 2
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "(<)"
-                            }
-                        ]
-        , test "should not report commutative operators (+) (*) (==) (/=) (&&) (||)" <|
-            \() ->
-                """module A exposing (..)
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error
+                        { message = "REPLACEME"
+                        , details = [ "REPLACEME" ]
+                        , under = "(<)"
+                        }
+                    ]
+    , test "should not report commutative operators (+) (*) (==) (/=) (&&) (||)" <|
+        \() ->
+            """module A exposing (..)
 a = (+) 1
 b = (*) 1
 c = (==) 1
@@ -59,18 +62,44 @@ d = (/=) 1
 e = (&&) True
 f = (||) True
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectNoErrors
-        ]
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    ]
 
 
-expectError : String -> Review.Test.ReviewResult -> Expectation
-expectError operator reviewResult =
-    Review.Test.expectErrors
-        [ Review.Test.error
-            { message = "REPLACEME"
-            , details = [ "REPLACEME" ]
-            , under = operator
-            }
+nonCommutativeOperatorTests : List Test
+nonCommutativeOperatorTests =
+    List.map
+        (\operator ->
+            test ("should report non-commutative operators " ++ operator) <|
+                \() ->
+                    ("""module A exposing (..)
+a = """ ++ operator ++ """ value
+""")
+                        |> Review.Test.run rule
+                        |> Review.Test.expectErrors
+                            [ Review.Test.error
+                                { message = "REPLACEME"
+                                , details = [ "REPLACEME" ]
+                                , under = operator
+                                }
+                            ]
+        )
+        [ "(-)"
+        , "(/)"
+        , "(//)"
+        , "(^)"
+        , "(<)"
+        , "(>)"
+        , "(<=)"
+        , "(>=)"
+        , "(++)"
+        , "(|>)"
+        , "(<|)"
+        , "(>>)"
+        , "(<<)"
+        , "(|.)"
+        , "(|=)"
+        , "(</>)"
+        , "(<?>)"
         ]
-        reviewResult
