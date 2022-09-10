28520f72b4013c2b2cfd4f065306fa3ca96e64fd
Jeroen Engels <jfm.engels@gmail.com>
2022-09-10 13:23:31 +0200

    Add test

--- tests/NoConfusingPrefixOperatorTest.elm
+++ tests/NoConfusingPrefixOperatorTest.elm
@@ -8,17 +8,31 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoConfusingPrefixOperator"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report fully-applied operators" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a = a < 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should not report operators not used in an application" <|
+            \() ->
+                """module A exposing (..)
+a = (<)
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when using a confusing operator with only a single argument" <|
+            \() ->
+                """module A exposing (..)
+a = (<) 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "(<)"
                             }
                         ]
         ]
